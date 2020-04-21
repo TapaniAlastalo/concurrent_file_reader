d7feaa2649ea999cfbd684362051cecb157784c2
Tapani Alastalo <tapani.alastalo@gmail.com>
2020-04-21 12:17:58 +0300

    tasks and results

---
+++ ConcurrentFileReader/ResultCollection.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConcurrentFileReader
+{
+    public class ResultCollection : IDisposable
+    {
+        public List<string> SQueue { get; set; }
+        public int Count { get; set; }
+        public long Sum { get; set; }
+        private readonly bool isLimited;
+        private int limit;
+
+        public ResultCollection()
+        {
+            SQueue = new List<string>();
+            isLimited = false;
+            int Count = 0;
+            long Sum = 0;
+        }
+        public ResultCollection(int maxCount)
+        {
+            SQueue = new List<string>();
+            isLimited = true;
+            limit = maxCount;
+            int Count = 0;
+            long Sum = 0;
+        }
+
+        public bool TryAdd(string text)
+        {
+            // check whether queue is not limited or there is still room
+            if (!isLimited || SQueue.Count < limit)
+            {
+                lock (SQueue)
+                {
+                    string[] values = text.Split();
+                    foreach(string value in values)
+                    {
+                        // Debug
+                        Console.WriteLine("TryAdd: " + value);
+                        SQueue.Add(value);
+                    }                    
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public bool TryTake(out string value)
+        {
+            lock (SQueue)
+            {
+                if (SQueue.Count > 0)
+                {
+                    value = SQueue[0];
+                    SQueue.RemoveAt(0);
+                    return true;
+                }
+                else
+                {
+                    value = "";
+                    return false;
+                }
+            }
+        }
+
+        public bool TryCountSum(out int value)
+        {
+            lock (SQueue)
+            {
+                value = 0;
+                bool success = true;
+                while (SQueue.Count > 0)
+                {
+                    try
+                    {
+                        value += int.Parse(SQueue[0]);
+                    }
+                    catch (Exception e)
+                    {
+                        value += 0;
+                        success = true;
+                    }
+                    finally
+                    {
+                        SQueue.RemoveAt(0);                        
+                    }
+                }
+                return success;
+            }
+        }
+
+        public int QueueCount()
+        {
+            return SQueue.Count();
+        }
+
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ConcurrentFileReader/TaskHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace ConcurrentFileReader
+{
+    public class TaskHandler
+    {
+        const int RUNNERS = 10; 
+        const int WAIT_DELAY = 10;
+        const int QUEUE_LIMIT = 1000*1000;
+
+        // FILE
+        const string PATH = "test_files\\test1.txt";
+        const int LENGTH = 10;
+
+        public void RunTasks()
+        {
+            // Create blocking queue for communication.
+            //using (var queue = new ResultCollection())
+            using (var results = new ResultCollection(QUEUE_LIMIT))
+            {
+                // Create tasks
+                var tasks = new List<Task>();
+                for (int i = 0; i < RUNNERS; i++)
+                {
+                    tasks.Add(ReadWriteAsync(i, results));
+                }
+
+                // Wait for all tasks to complete
+                Task.WaitAll(tasks.ToArray());
+
+                // Results
+                CheckResults(results);
+            }
+        }
+
+        private async Task ReadWriteAsync(int counter, ResultCollection results)
+        {
+            // Read File
+            Console.WriteLine("START TASK-" + counter);
+
+            // just wait
+            await WaitABit();
+            
+            int offset = counter * LENGTH;
+            string foundText = FileHandler.ReadFromFile(PATH, offset, LENGTH);
+            
+            // DEBUG
+            Console.WriteLine("TASK-" + counter + " Read: " + foundText);
+
+            
+            // just wait
+            await WaitABit();                
+            if (!results.TryAdd(foundText))
+            {
+                Console.WriteLine($"TASK-{counter}: Couldn't add text. Queue length: {results.SQueue.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"TASK-{counter}: Added text. Queue length: {results.SQueue.Count}");
+            }
+        }
+
+        private void CheckResults(ResultCollection results)
+        {
+            if (!results.TryCountSum(out int value))
+            {
+                Console.WriteLine($"\t\t\t\t\t\t\tCHECK-OUT: Couldn't take correct value: SUM = {value} , from results. Queue length: {results.SQueue.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"\t\t\t\t\t\t\tCHECK-OUT: Success: SUM = {value}. Queue length: {results.SQueue.Count}");
+            }
+
+        }
+
+        private async Task WaitABit()
+        {
+            await Task.Delay(WAIT_DELAY);
+        }
+    }
+}
